cd969022d4951ec71a7273be232a12a975868152
tarek <tarek.graphicdesign@gmail.com>
2018-10-04 10:14:55 +0200

    chore: add font loader to webpack configuration and font example

--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,7 @@ module.exports = {
     new webpack.DefinePlugin(Object.assign({
       'process.env': { NODE_ENV: JSON.stringify(mode) },
       __DEV__
-    }, {/* GLOBALS */}))
+    }, {/* GLOBALS */ }))
   ]/* ,
   devServer: {
     contentBase: './dist',
@@ -66,6 +66,30 @@ module.exports.module.rules.push({
     'css-loader', // translates CSS into CommonJS
     'sass-loader' // compiles Sass to CSS, using Node Sass by default
   ]
+});
+
+// Fonts
+// ------------------------------------
+[
+  ['woff', 'application/font-woff'],
+  ['woff2', 'application/font-woff2'],
+  ['otf', 'font/opentype'],
+  ['ttf', 'application/octet-stream'],
+  ['eot', 'application/vnd.ms-fontobject'],
+  ['svg', 'image/svg+xml'],
+].forEach((font) => {
+  const extension = font[0];
+  const mimetype = font[1];
+
+  module.exports.module.rules.push({
+    test: new RegExp(`\\.${extension}$`),
+    loader: 'url-loader',
+    options: {
+      name: 'fonts/[name].[ext]',
+      limit: 10000,
+      mimetype,
+    },
+  });
 });
 
 // Images
