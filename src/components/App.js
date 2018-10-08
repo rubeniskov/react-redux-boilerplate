4a75607d9c835c8a42f862a4f0f2880f2c647e5b
Rubén López Gómez <loslopez1@icloud.com>
2018-10-08 12:38:59 +0200

    refactor: App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,28 @@
 import React from 'react';
-import {Provider} from 'react-redux';
-import UserLoadButton from './UserLoadButton';
-import UserViewData from './UserViewData';
+import {Provider as StoreProvider} from 'react-redux';
+import {Router, Switch, Route} from 'react-router';
+import LoaderProvider from './LoaderProvider';
 
-export default ({store, routes, ...rest}) => (
-  <Provider store={store}>
-    <div>
-      <UserViewData />
-      <br />
-      <UserLoadButton />
-    </div>
-  </Provider>);
+export default ({
+  store, routes, history, layout, loader, ...rest
+}) => (
+  <StoreProvider store={store}>
+    <LoaderProvider loader={loader}>
+      <Router history={history}>
+        <Switch>
+          {routes.map((route, key) => {
+            const {component, layout, ...props} = route;
+            const Layout = layout || (({children}) => (<div>{children}</div>));
+            const Component = component;
+            return (
+              <Route
+                key={key}
+                component={() => (<Layout><Component /></Layout>)}
+                {...props} />
+            );
+          })}
+        </Switch>
+      </Router>
+    </LoaderProvider>
+  </StoreProvider>
+);
